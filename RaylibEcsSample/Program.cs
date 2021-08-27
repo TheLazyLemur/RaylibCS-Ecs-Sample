1bb69c7342cfd139626d7c8c27ba0c82a51335ff
Dan <danrousseau@protonmail.com>
2021-08-27 21:48:19 +0200

    Moved Program.cs to use toplevel statements

--- RaylibEcsSample/Program.cs
+++ RaylibEcsSample/Program.cs
@@ -5,47 +5,37 @@ using RaylibEcsSample.Components;
 using RaylibEcsSample.Systems;
 using static Raylib_cs.Raylib;
 
-namespace RaylibEcsSample
+InitWindow(1920, 1080, "Sample Project");
+SetTargetFPS(60);
+var camera = new Camera2D
 {
-    internal static class Program
-    {
-        private static void Main()
-        {
-            InitWindow(1920, 1080, "Sample Project");
-            SetTargetFPS(60); 
-            var camera = new Camera2D
-            {
-                target = new Vector2(),
-                offset = new Vector2(),
-                rotation = 0.0f,
-                zoom = 1.0f
-            };
-            
-            var world = new EcsWorld();
+    target = new Vector2(),
+    offset = new Vector2(),
+    rotation = 0.0f,
+    zoom = 1.0f
+};
 
-            for (var i = 0; i < 100000; i++)
-            {
-                var entity = world.NewEntity();
-                entity.Get<DrawComponent>();
-                entity.Get<TransformComponent>();
-            }
+var world = new EcsWorld();
 
-            var systems = new EcsSystems(world);
-            systems.Add(new TransformSystem()).Add(new DrawSystem()).Init();
+for (var i = 0; i < 100000; i++)
+{
+    var entity = world.NewEntity();
+    entity.Get<DrawComponent>();
+    entity.Get<TransformComponent>();
+}
 
-            
-            while (!WindowShouldClose())
-            {
-                ClearBackground(Color.BLACK);
-                BeginMode2D(camera);
-                DrawFPS(1920/2,10); 
-                
-                systems.Run();
-                
-                EndMode2D();
-                EndDrawing();
-            }
-        }
+var systems = new EcsSystems(world);
+systems.Add(new TransformSystem()).Add(new DrawSystem()).Init();
 
-    }
+
+while (!WindowShouldClose())
+{
+    ClearBackground(Color.BLACK);
+    BeginMode2D(camera);
+    DrawFPS(1920 / 2, 10);
+
+    systems.Run();
+
+    EndMode2D();
+    EndDrawing();
 }
